515321644ceb8ec6ffb226b4d26a1bf10388db6e
jacksondouglasti <jacksondouglasti@gmail.com>
2019-11-21 11:57:07 -0300

    Added Question and Answer widgets

--- flutter_quiz_app/lib/main.dart
+++ flutter_quiz_app/lib/main.dart
@@ -1,20 +1,57 @@
 import 'package:flutter/material.dart';
+import './question.dart';
+import './answer.dart';
 
 void main() => runApp(MyApp());
 
-class MyApp extends StatelessWidget {
+class MyApp extends StatefulWidget {
+  @override
+  State<StatefulWidget> createState() {
+    return _MyAppState();
+  }
+}
+
+class _MyAppState extends State<MyApp> {
+  var _questionIndex = 0;
+
+  void _answerQuestion() {
+    setState(() {
+      ++_questionIndex;
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
+    var questions = [
+      {
+        'questionText': 'What\'s your favorite color?',
+        'answers': ['Black', 'Red', 'Green', 'White']
+      },
+      {
+        'questionText': 'What\'s your favorite animal?',
+        'answers': ['Rabbit', 'Snake', 'Elephant', 'Lion']
+      },
+      {
+        'questionText': 'What\'s your favorite food?',
+        'answers': ['Pasta', 'Meat', 'Beans', 'X-Burger']
+      }
+    ];
+
     return MaterialApp(
       theme: ThemeData(
         primarySwatch: Colors.blue,
       ),
       home: Scaffold(
           appBar: AppBar(
-            title: Text("Quiz App"),
+            title: Text('Quiz App'),
           ),
           body: Column(
-            children: <Widget>[],
+            children: [
+              Question(questions[_questionIndex]['questionText']),
+              ...(questions[_questionIndex]['answers'] as List<String>).map((answer) {
+                return Answer(_answerQuestion, answer);
+              }).toList()
+            ],
           )),
     );
   }
