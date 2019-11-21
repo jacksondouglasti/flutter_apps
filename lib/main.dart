c2b84b060c297e6791c7a609cf47b8227161397d
jacksondouglasti <jacksondouglasti@gmail.com>
2019-11-21 10:23:28 -0300

    Project created

---
+++ lib/main.dart
@@ -0,0 +1,21 @@
+import 'package:flutter/material.dart';
+
+void main() => runApp(MyApp());
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: Scaffold(
+          appBar: AppBar(
+            title: Text("Quiz App"),
+          ),
+          body: Column(
+            children: <Widget>[],
+          )),
+    );
+  }
+}
